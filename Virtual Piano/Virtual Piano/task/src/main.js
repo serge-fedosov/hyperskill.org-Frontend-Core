8be6fe27206795606e79d1bf3dd80cb09fd3c82a
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-10-05 12:25:22 +0400

    Virtual Piano - Stage 4/5

--- Virtual Piano/Virtual Piano/task/src/main.js
+++ Virtual Piano/Virtual Piano/task/src/main.js
@@ -2,7 +2,8 @@ function onLoad() {
     document.addEventListener("keydown", function(event) {
         if (event.code === "KeyA" || event.code === "KeyS" || event.code === "KeyD" || event.code === "KeyF" ||
                 event.code === "KeyG" || event.code === "KeyH" || event.code === "KeyJ") {
-            console.log("The \'" + event.key + "\' key is pressed.");
+            let audio = new Audio(`mp3/${event.key}.mp3`);
+            audio.play();
         }
     });
 }
